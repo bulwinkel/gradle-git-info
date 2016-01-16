32d733482fee6ed8ae6e02e3c8263d81774f90cc
bulwinkel <kelvin@bulwinkel.com>
2016-01-16 16:24:50 +1000

    created GitInfo class and exposed it to the project

--- src/main/groovy/com/bulwinkel/gitinfo/GitInfoPlugin.groovy
+++ src/main/groovy/com/bulwinkel/gitinfo/GitInfoPlugin.groovy
@@ -1,10 +1,12 @@
 package com.bulwinkel.gitinfo
 
+import com.bulwinkel.gitinfo.internal.GitInfo
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class GitInfoPlugin implements Plugin<Project> {
-  @Override
-  void apply(Project project) {
+
+  @Override void apply(Project project) {
+    project.ext.gitInfo = GitInfo()
   }
 }
---
+++ src/main/groovy/com/bulwinkel/gitinfo/internal/GitInfo.groovy
@@ -0,0 +1,26 @@
+package com.bulwinkel.gitinfo.internal
+
+final class GitInfo {
+
+  @Lazy final int commitCount = {
+    def p = Runtime.getRuntime().exec("git rev-list --all --count")
+
+    def result = p.waitFor()
+    if (result != 0) {
+      return 0 // no git revisions
+    }
+    return p.getInputStream().readLines().get(0).toInteger()
+  }()
+
+  @Lazy final String latestTag = {
+    def p = Runtime.getRuntime().exec("git describe --tags")
+
+    def result = p.waitFor()
+    if (result != 0) {
+      println "WARNING: No git tags in current git project"
+      return "" // no git tag
+    }
+    return p.getInputStream().readLines().get(0).toString().trim()
+  }()
+
+}
